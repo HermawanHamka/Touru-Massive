f5d28eaa0290b1824b65366a85ea673b87ab8212
Muhammad Farizul Imami Sudirman <ozuxekyosuke@gmail.com>
2023-06-08 18:56:57 +0700

    list tourguide selesai

---
+++ app/src/main/java/com/example/myapplication/adapter/ListHeroAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.Hero
+import com.example.myapplication.R
+import java.util.*
+
+
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
+        }
+
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.tvGambar)
+        val tvName: TextView = itemView.findViewById(R.id.tv_nama)
+        val tvDescription: TextView = itemView.findViewById(R.id.harga)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/model/Hero.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Created by sidiqpermana on 10/29/16.
+ */
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable
