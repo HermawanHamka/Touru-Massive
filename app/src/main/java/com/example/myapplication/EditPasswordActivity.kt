535235eb64f93ce4d240a3c4fd111b9314b115eb
Dendito <ananditorafip22@gmail.com>
2023-06-26 12:38:11 +0700

    menambahkan button active di edit sandi dan username

--- app/src/main/java/com/example/myapplication/EditPasswordActivity.kt
+++ app/src/main/java/com/example/myapplication/EditPasswordActivity.kt
@@ -5,13 +5,30 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class EditPasswordActivity : AppCompatActivity() {
+
+    private lateinit var btnUbah: Button
+    private lateinit var editBaru: EditText
+    private lateinit var editKonfirm: EditText
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_password)
+
+        /* get id */
+        btnUbah = findViewById(R.id.btnUbahPass)
+        editBaru = findViewById(R.id.editPassBaru)
+        editKonfirm = findViewById(R.id.editPassKonfirmasi)
+
+        editBaru.addTextChangedListener(loginTextWatcher)
+        editKonfirm.addTextChangedListener(loginTextWatcher)
 
         val backButton: ImageView = findViewById(R.id.imgBackUbahSandi)
         backButton.setOnClickListener {
@@ -19,6 +36,21 @@ class EditPasswordActivity : AppCompatActivity() {
             intent.putExtra("checkbox_status", true)
             setResult(Activity.RESULT_OK, intent)
             finish()
+        }
+    }
+    private val loginTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val editBaru = editBaru.text.toString().trim()
+            val editKonfirm = editKonfirm.text.toString().trim()
+
+            btnUbah.isEnabled = editBaru.isNotEmpty()
+            btnUbah.isEnabled = editKonfirm.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
         }
     }
 }
--- app/src/main/java/com/example/myapplication/EditUsernameActivity.kt
+++ app/src/main/java/com/example/myapplication/EditUsernameActivity.kt
@@ -4,6 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.myapplication.helper.Constant
@@ -11,10 +15,11 @@ import com.example.myapplication.helper.PreferenceHelper
 
 class EditUsernameActivity : AppCompatActivity() {
     lateinit var sharedPref : PreferenceHelper
+    private lateinit var editUsername: EditText
+    private lateinit var btnUbahUsername: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_username)
-
         sharedPref = PreferenceHelper(this)
 
         val backButton: ImageView = findViewById<ImageView>(R.id.imgBackUbahUsername)
@@ -28,5 +33,21 @@ class EditUsernameActivity : AppCompatActivity() {
         val ubahUsername = findViewById<TextView>(R.id.usernameTerdaftar)
 
         ubahUsername.text = sharedPref.getString(Constant.PREF_USERNAME)
+        editUsername = findViewById(R.id.ubahUsername)
+        btnUbahUsername= findViewById(R.id.btnUbahUsername)
+        editUsername.addTextChangedListener(loginTextWatcher)
+    }
+    private val loginTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val editUsername = editUsername.text.toString().trim()
+
+            btnUbahUsername .isEnabled = editUsername.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
     }
 }
