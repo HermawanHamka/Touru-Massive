573e1dc73bbda090bd609312a4d45b6349607dc8
Dendito <ananditorafip22@gmail.com>
2023-06-26 16:51:30 +0700

    Menambahkan search view didestinasi fragment

--- app/src/main/java/com/example/myapplication/adapter/DestinasiAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/DestinasiAdapter.kt
@@ -5,40 +5,84 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.model.DataDestination
+import java.util.*
+import kotlin.collections.ArrayList
 
-class DestinasiAdapter(val results: ArrayList<DataDestination>, val listener: OnAdapterListener)
-    : RecyclerView.Adapter<DestinasiAdapter.ViewHolder> () {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder (
-        LayoutInflater.from(parent.context).inflate(R.layout.card_item_destinasi, parent, false)
-    )
+class DestinasiAdapter
+    (private var originalData: ArrayList<DataDestination>,
+     private var filteredData: ArrayList<DataDestination>,
+     private val listener: OnAdapterListener
 
-    override fun getItemCount() = results.size
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val result = results[position]
-        holder.view.findViewById<TextView>(R.id.namaDestinasi).text = result.title
-        Log.d("DestinasiAdapter","resultImage: ${result.photo}")
-        val url = "http://192.168.0.103:3000${result.photo}"
-        Glide.with(holder.view)
-            .load(url)
-            .placeholder(R.drawable.grey_background)
-            .error(R.drawable.grey_background)
-            .centerCrop()
-            .into(holder.view.findViewById(R.id.gambarDestinasi))
-        holder.view.setOnClickListener{
-            listener.onClick( result )
+     ) : RecyclerView.Adapter<DestinasiAdapter.ViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_item_destinasi, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return filteredData.size
+    }
+
+    override fun onBindViewHolder(holder: DestinasiAdapter.ViewHolder, position: Int) {
+        val result = filteredData[position]
+        holder.bind(result)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val namaDestinasi: TextView = itemView.findViewById(R.id.namaDestinasi)
+        private val gambarDestinasi: ImageView = itemView.findViewById(R.id.gambarDestinasi)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val result = filteredData[position]
+                    listener.onClick(result)
+                }
+            }
+        }
+
+
+        fun bind(data: DataDestination) {
+            namaDestinasi.text = data.title
+            val url = "http://192.168.100.7:3000${data.photo}"
+            Glide.with(itemView)
+                .load(url)
+                .placeholder(R.drawable.grey_background)
+                .error(R.drawable.grey_background)
+                .centerCrop()
+                .into(gambarDestinasi)
         }
     }
-    class ViewHolder (val view: View) : RecyclerView.ViewHolder(view)
-
     @SuppressLint("NotifyDataSetChanged")
     fun setData(data: ArrayList<DataDestination>) {
-        results.clear()
-        results.addAll(data)
+        originalData = data
+        filterData("") // Apply initial filtering
+    }
+    fun filterData(query: String) {
+        filteredData.clear()
+        if (query.isEmpty()) {
+            filteredData.addAll(originalData) // Show all data if query is empty
+        } else {
+            val searchQuery = query.lowercase(Locale.getDefault())
+            for (item in originalData) {
+                if (item.title?.lowercase(Locale.getDefault())?.contains(searchQuery) == true) {
+                    filteredData.add(item)
+                } else if (item.city?.lowercase(Locale.getDefault())
+                        ?.contains(searchQuery) == true
+                ) {
+                    filteredData.add(item)
+                }
+            }
+        }
         notifyDataSetChanged()
     }
     interface OnAdapterListener {
--- app/src/main/java/com/example/myapplication/fragment_menu/DestinasiFragment.kt
+++ app/src/main/java/com/example/myapplication/fragment_menu/DestinasiFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.model.DestinasiModel
@@ -24,7 +25,9 @@ import retrofit2.Response
 class DestinasiFragment : Fragment() {
     private val TAG: String = "DestinasiFragment"
     private  lateinit var destinasiAdapter: DestinasiAdapter
-
+    private lateinit var searchView: SearchView
+    private val originalData: ArrayList<DataDestination> = ArrayList()
+    private val filteredData: ArrayList<DataDestination> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,17 +35,21 @@ class DestinasiFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_destinasi, container, false)
+    }
 
-
+    override fun onStart() {
+        super.onStart()
+        setupRecyclerView()
+        getDataFromApi()
     }
 
     private fun setupRecyclerView() {
-        val recyclerView by lazy { find<RecyclerView>(R.id.rv_heroes_homestay) }
-        destinasiAdapter = DestinasiAdapter(arrayListOf(), object : DestinasiAdapter.OnAdapterListener {
+        val recyclerView by lazy { find<RecyclerView>(R.id.rv_heroes_destinasi) }
+        destinasiAdapter = DestinasiAdapter(originalData, filteredData,object : DestinasiAdapter.OnAdapterListener {
             override fun onClick(results: DataDestination) {
                 startActivity(
                     Intent(context, DetailDestinasi::class.java)
-                        .putExtra("photo", "http://192.168.0.102:3000${results.photo}")
+                        .putExtra("photo", "http://192.168.100.7:3000${results.photo}")
                         .putExtra("title", results.title)
                         .putExtra("desc", results.desc)
                         .putExtra("city", results.city)
@@ -52,6 +59,7 @@ class DestinasiFragment : Fragment() {
             }
 
         })
+
         recyclerView.apply {
             recyclerView.layoutManager = GridLayoutManager(context,2, GridLayoutManager.VERTICAL, false)
             recyclerView.adapter = destinasiAdapter
@@ -59,11 +67,22 @@ class DestinasiFragment : Fragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onStart()
-        setupRecyclerView()
-        getDataFromApi()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        searchView = view.findViewById(R.id.search_view)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                destinasiAdapter.filterData(query ?: "")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                destinasiAdapter.filterData(newText ?: "")
+                return true
+            }
+        })
 
     }
     private fun getDataFromApi() {
